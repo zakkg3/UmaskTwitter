b43f6d73109beea99993e2a4e7cabfdb7ee259dd
Nicolas Kowenski <nico@garufa.ethz.ch>
2019-08-27 13:00:38 +0200

    config

---
+++ umask_config.py
@@ -0,0 +1,19 @@
+# Twitter username and password of the bot
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = "-"
+ACCESS_TOKEN_SECRET = ""
+
+# Debugging mode
+# If this option is True then extra debugging info will be printed
+debug = True
+
+
+busqueda= '#EnBairesVotoAMatias'
+
+
+# Flat text files containing various
+ignore_list='ignore.txt'   # List of twitter screen names to ignore
+filtered_word_list='filter.txt'    # Ignore tweets if they contain these words
+
+last_id_file='lastid.txt'  # Last ID the bot "saw"
